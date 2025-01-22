9b04f2ffdff8921cc5f32b8b497cdd201f19c3fa
Abhi9av-Git <abhinav230333@gmail.com>
2025-01-22 19:28:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score="33"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+let ans=null
+
+console.log(typeof ans);
+
+let value=Number(ans)
+
+console.log(typeof value);
+
+console.log(value);
+
+// after conversion of other datatypes to number 
+// "33"=> 33
+// "33abc"=> NaN
+// true=>1; false=0;
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false;
+// ""=>false
+// "aditya"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
