2c3ee5827dc3f85568ec6e6ccea92d321cbb499f
Abhi9av-Git <abhinav230333@gmail.com>
2025-01-22 18:59:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+let name="hitesh"
+
+let age=18
+
+let isLoggedIn=false
+
+/*
+all Kinds of data types
+
+number=> ranges from 2 to 2 raise to the power 53
+bigint 
+string=""
+boolean=> true/false
+null for ex= let state=null
+undefined=>
+symbol => unique
+
+
+main data type=> object
+*/
+
+console.log(typeof "Abhinav");
+
+console.log(typeof null);  // object
+
+console.log(typeof undefined);  // undefined
