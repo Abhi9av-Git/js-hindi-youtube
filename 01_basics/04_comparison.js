dcf0fdc10e2a09c0a41a4162f7ecb6c004dddea5
Abhi9av-Git <abhinav230333@gmail.com>
2025-01-23 18:05:49 +0000

    Comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+console.log(2>1);
+
+let val1=7;
+
+let val2=10;
+
+if (val1>=val2) {
+
+  console.log("val1 is greater or equal");
+}
+else {
+
+  console.log("val2 is greater");
+}
+
+// console.log(null>0); // gives false as output
+// console.log(null==0); // gives false as output
+// console.log(null>=0); // gives true as output bcz here before comparison null gets converted into 0
+
+// console.log(undefined>0); // gives false as output
+// console.log(undefined<0); // gives false as output
+// console.log(undefined==0); // gives false as output
+
+// === not only checks the value but also checks the datatype
+
+console.log("2"===2);
+
