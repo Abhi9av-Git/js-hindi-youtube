5ba9b4bccd00e836b60dbc1b70025876fa44cc68
Abhi9av-Git <abhinav230333@gmail.com>
2025-01-23 18:05:26 +0000

    conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -40,3 +40,46 @@ let stringNumber=String(someNumber)
 console.log(typeof stringNumber);
 
 console.log(stringNumber);
+
+// ************** Operations ***********
+
+let val=3;
+
+let negVal=-val;
+
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 raise to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello";
+
+let str2=" Aditya";
+
+let str3=str1+str2;
+
+console.log(str3);
+
+// Don't try to write codes like below four commented codes, its just for example
+//console.log("1" +2);
+
+//console.log(1+ "2");
+
+//console.log("1"+ 2 +2); // if string is first then every one is treated as string
+
+//console.log(1+ 2 +"2"); // if string is at last then operation is performed first and then converted into string
+
+console.log(true); // gives true as output
+
+console.log(+true); // gives 1 as output
+
+let gameCounter=100;
+
+gameCounter++;
+
+console.log(gameCounter);
+
